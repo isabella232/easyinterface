76c4c2a452d24ed853f39b4d9718ce50bc3f2c6e
Einar Broch Johnsen <einarj@ifi.uio.no>
2017-03-29 16:57:17 +0200

    Initial easyinterface for Encore

---
+++ server/bin/encore/encorebackend.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+. misc/parse_params.sh
+
+execroot=$(getparam "execroot")
+files=$(getparam "files")
+threads=$(getparam "threadselect")
+optimlevel=$(getparam "optimlevel")
+outdir=$execroot/_ei_tmp
+export HOME=/Users/einar
+
+echo "<eiout>"
+echo "<eicommands>"
+
+# COMPILE
+# /Users/einar/git/encore/release/encorec $files -o $outdir/a.out --verbose > $outdir/stdout 
+/Users/einar/git/encore/release/encorec $files -o $outdir/a.out -O$optimlevel  &> $outdir/err 
+
+# RUN OR PRINT ERROR IF COMPILATION FAILED
+if [ $? == 0 ]; then
+
+
+    echo "<printonconsole>"
+    echo "<content format='text' execid='$execid' ext='out' refreshrate='$refresh' action='append'>"
+    echo "The source files were successfully compiled to Encore!"
+    echo "It is executed with the following number of ponythreads: $threads"
+    echo "Optimisation level for the C compiler: $optimlevel"
+    echo "</content>"
+    echo "</printonconsole>"
+
+    echo "<printonconsole consoleid='encexec' consoletitle='Encore output'>"
+    echo "<content format='text' execid='$execid' ext='out' refreshrate='$refresh' action='append'>"
+    echo ""
+    echo "Here is the result of execution."
+    echo ""
+    pwd
+    time $outdir/a.out --ponythreads $threads
+    echo "</content>"
+    echo "</printonconsole>"
+    echo "</eicommands>"
+
+else
+    echo "<printonconsole>"
+    echo "<content format='text'><![CDATA[ There are some errors!"
+    cat $outdir/err
+    echo "]]></content>"
+    echo "</printonconsole>"
+    echo "</eicommands>"
+fi
+
+echo "</eiout>"
---
+++ server/bin/encore/encorecompiler.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+. misc/parse_params.sh
+
+execroot=$(getparam "execroot")
+files=$(getparam "files")
+others=$(getparam "others")
+outdir=$execroot/_ei_tmp
+export HOME=/Users/einar
+
+echo "<eiout>"
+echo "<eicommands>"
+
+# COMPILE
+# /Users/einar/git/encore/release/encorec $files -o $outdir/a.out --verbose > $outdir/stdout 
+# /Users/einar/git/encore/release/encorec $files -tc
+
+# RUN OR PRINT ERROR IF COMPILATION FAILED
+if [ $? == 0 ]; then
+
+    echo "<printonconsole>"
+    echo "<content format='text' execid='$execid' ext='out' refreshrate='$refresh' action='append'>"
+    echo "Compiling into Encore."
+    echo ""
+if [ $others == verbose ]; then 
+    /Users/einar/git/encore/release/encorec $files -tc --verbose
+else
+    /Users/einar/git/encore/release/encorec $files -tc
+fi
+    echo "</content>"
+    echo "</printonconsole>"
+fi
+    echo "</eicommands>"
+    echo "</eiout>"
